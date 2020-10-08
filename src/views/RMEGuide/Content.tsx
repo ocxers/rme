5cafbb9df779bed4827338cd2c50d3b0eb198b00
ShengXing Chi <ocxers@gmail.com>
2020-10-08 08:04:16 +0800

    feat: 编写demo

--- src/views/RMEGuide/Content.tsx
+++ src/views/RMEGuide/Content.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import RME from 'react-magic-element'
 import RMERoutes from './routes'
@@ -7,12 +7,90 @@ import _ from 'lodash'
 
 export default (props: any) => {
   const location = useLocation()
-  const renderDefaultStylesContent = () => {
-    if (location.pathname === '/rme') {
+  const [rmeProps, setRmeProps] = useState('')
+  const [computeRmeProps, setComputeRmeProps] = useState({} as any)
+  const [propTags, setPropTags] = useState({} as any)
+  const handleRmePropsChanged = (ev: any) => {
+    setRmeProps(ev.target.value)
+  }
+  const handleAddProps = () => {
+    if (rmeProps) {
+      const keyValue = _.compact(rmeProps.split('='))
+      if (keyValue.length) {
+        const rmePropsObj: any = _.cloneDeep(propTags)
+        rmePropsObj[keyValue[0]] = keyValue[1] ? keyValue[1] : true
+
+        setPropTags(rmePropsObj)
+      }
+    }
+    setRmeProps('')
+  }
+  useEffect(() => {
+    const rmePropTags: any = {}
+    _.map(_.compact('mb15  b bhw14 bvw35 bs-double bc-success btc-danger blc-info r-50'.split(' ')), (key: string) => {
+      rmePropTags[key] = true
+    })
+
+    setPropTags(rmePropTags)
+  }, [])
+
+  useEffect(() => {
+    // @ts-ignore
+    let computedPropTags = RME.computeProps(propTags)
+    setComputeRmeProps(computedPropTags)
+  }, [propTags])
+  const renderContent = () => {
+    let tagName = computeRmeProps.tagName || 'div'
+    if (computeRmeProps.className) {
+      delete computeRmeProps.tagName
+      if (tagName === 'input') {
+        return (
+          <input type={props.type || 'text'}
+                 {...computeRmeProps}
+          />
+        )
+      }
+
+      return React.createElement(tagName, computeRmeProps, [<React.Fragment key={Date.now()}>RME</React.Fragment>])
+    }
+  }
+  const handleRemoveTag = (tag: any) => {
+    let tempPropTags = _.cloneDeep(propTags)
+    delete tempPropTags[tag]
+    setPropTags(tempPropTags)
+  }
+  const renderTag = (key: string, val: any) => {
+    if (_.isBoolean(val)) {
+      return key
+    } else {
+      return `${key}=${val}`
+    }
+  }
+  const renderPropTags = () => {
+    return _.map(_.keys(propTags), (key: any, idx: number) => {
       return (
-        <RME p30>
+        <RME tag key={idx} primary mb10>{renderTag(key, propTags[key])} <RME span onClose={() => {
+          handleRemoveTag(key)
+        }}></RME></RME>
+      )
+    })
+  }
+  const renderDefaultStylesContent = () => {
+    if (location.pathname === '/rme' || location.pathname === '/rme/') {
+      return (
+        <RME ph30>
           <RME h1>Welcome to RME!</RME>
-          <RME>It's a personal small library to speed up a little.</RME>
+          {/*<RME mb30>It's a personal small library to speed up a little.</RME>*/}
+          <RME center txt-center mb30>
+            <RME row mb30>
+              <RME input big placeholder={'Input some props'} value={rmeProps} onChange={handleRmePropsChanged} />
+            </RME>
+            <RME btn primary onClick={handleAddProps}>Add Props</RME>
+          </RME>
+          <RME b-warning r5 p15 mb30>
+            <RME bg={'#efefef'} r5 p15 mb15>{renderPropTags()}</RME>
+            {renderContent()}
+          </RME>
         </RME>
       )
     }
--- src/views/RMEGuide/Header.tsx
+++ src/views/RMEGuide/Header.tsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import cx from 'classnames'
 import RME from 'react-magic-element'
+
 const logo = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K'
 export default (props: any) => {
   return (
     <RME header flex fixed h60 w-100 center className={cx('box-bottom-shadow')}>
-      <RME a row ph15 w-100 href='/rme'>
-        <RME span w30 h30 mr15>
-          <img src={logo} alt=""/>
+      <RME row ph15 w-100>
+        <RME a row href='/rme'>
+          <RME span w30 h30 mr15>
+            <img src={logo} alt="" />
+          </RME>
+          <RME span fs24 fc='white' fw700>React Magic Element</RME>
         </RME>
-        <RME span fs24 fc='white' fw700>React Magic Element</RME>
         <RME rest></RME>
       </RME>
     </RME>
